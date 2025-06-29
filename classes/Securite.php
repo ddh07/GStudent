306d5743f9b945604966f0d53fca7bd493e65c00
ddh07 <nazaireodg07@gmail.com>
2025-06-29 08:09:36 +0000

    Ajout de la validation des date et des images

--- classes/Securite.php
+++ classes/Securite.php
@@ -1,27 +1,26 @@
 <?php
+    require 'Config.php';
 /**
  * [Description Securite]
  */
-class Securite
-{
+class Securite{
+    private $extension = null;
+    private $config = null;
+
+    function __construct(){
+        try{
+            $this->config = new Config();
+        }catch(Exception $e){
+            echo "erreur ".$e->getMessage();
+        }
+    }
+
     /**
-     * Nettoyer une chaîne (anti-XSS)
+     * anti-XSS
      */
     public static function validateData($data)
     {
         return htmlspecialchars(strip_tags(trim($data)), ENT_QUOTES, 'UTF-8');
-    }
-
-    /**
-     * Nettoyer un tableau entier de données
-     */
-    public static function validateTableContent(array $donnees)
-    {
-        $resultat = [];
-        foreach ($donnees as $cle => $valeur) {
-            $resultat[$cle] = self::validateData($valeur);
-        }
-        return $resultat;
     }
 
     /**
@@ -55,4 +54,53 @@ class Securite
     {
         return password_verify($motDePasse, $hash);
     }
+
+    /**
+     * @param mixed $date
+     * Vérifie le format YYYY-MM-DD
+     * @return [type]
+     */
+    public static function validateDate($date){
+        $d = DateTime::createFromFormat('Y-m-d', $date);
+        return $d && $d->format('Y-m-d') === $date;
+    }
+
+    /**
+     * @param mixed $fichier
+     * Vérifie l'erreur
+     * Vérifie la taille
+     * Vérifie l’extension
+     * Vérifie le type MIME
+     * @return [type]
+     */
+    function validateFileImage($fichier){
+        try{
+            $config = new Config();
+            if ($fichier['erreur'] !== UPLOAD_ERR_OK) {
+                return "Erreur lors de l'upload.";
+            }
+
+            if ($fichier['size'] > $config->getimagesizeAutorize()) {
+                return "Fichier trop volumineux.";
+            }
+
+            $extension = strtolower(pathinfo($fichier['name'], PATHINFO_EXTENSION));
+            if (!in_array($extension, $config->getExtensionsAutorize())) {
+                 return "Extension non autorisée.";
+            }
+
+            $finfo = finfo_open(FILEINFO_MIME_TYPE);
+            $typeMime = finfo_file($finfo, $fichier['tmp_name']);
+            finfo_close($finfo);
+
+            if (!in_array($typeMime, $config->getMimesAutorize())) {
+                return "Type MIME non autorisé.";
+            }
+
+        }catch(Exception $e){
+            echo "erreur ".$e->getMessage();
+        }
+    
+    return "OK";
+}
 }
