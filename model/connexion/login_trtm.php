a312b8dbbd47acefc3c1b5c8fc4296e1bfa76bce
ddh07 <nazaireodg07@gmail.com>
2025-07-01 02:25:35 +0000

    Prise en charge des role de connexion

--- model/connexion/login_trtm.php
+++ model/connexion/login_trtm.php
@@ -8,6 +8,9 @@ session_start();
 require '../../classes/Connexion.php';
 require '../../classes/Securite.php';
 require '../../classes/Utilisateur.php';
+require '../../classes/Role.php';
+
+require '../../logs/logger.php';
 
 $pdo = Connexion::getConnection();
 
@@ -17,16 +20,35 @@ if ($_SERVER['REQUEST_METHOD'] === 'POST') {
 
     if (!empty($username) && !empty($password)) {
         $userModel = new Utilisateur($pdo);
-        $user = $userModel->getUtilisateurByUsername($username); // méthode à créer dans la classe Utilisateur
+        $user = $userModel->getUtilisateurByUsername($username);
 
         if ($user && Securite::validatePassword($password,$user["password"])) {
             $_SESSION['id_utilisateur'] = $user['id_utilisateur'];
             $_SESSION['username'] = $user['username'];
             $_SESSION['id_role'] = $user['id_role'];
 
+            $roleModel = new Role($pdo);
+            $role = $roleModel->getRoleById($user['id_role']);
+
+            switch($role['libelle_role']){
+                case "Administreur":
+                    header('Location: /vue/admin/Acceuil.php');
+                    break;
+                case "Secrétaire":
+                    header('Location: /vue/secretaire/Acceuil.php');
+                    break;
+                case "Professeur":
+                    header('Location: /vue/prof/Acceuil.php');
+                    break;
+                default:
+            }
+
             echo "Connexion réussie. Bienvenue, " . $user['username'];
+            logger::log("Tentative de connexion a l'utilisateur ".$username." reussite.");
+            header('Location: /vue/admin/acceuil_admin.php');
         } else {
             echo "username ou mot de passe incorrect.";
+            logger::log("Echec de tentative de connexion a l'utilisateur ".$username);
         }
     } else {
         echo "Veuillez remplir tous les champs.";
